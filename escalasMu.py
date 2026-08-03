c3c2581d5fabf932dcf2f9d43e9ab5373136c232
lapizzepa <larissagomez091@gmail.com>
2026-08-03 12:54:36 -0600

    Bemoles function

--- escalasMu.py
+++ escalasMu.py
@@ -11,6 +11,16 @@ def sostenidos(escalaMayoresSostenidos):
             print(f"Nota incorrecta: {escalaElegida[i]}")
         i += 1
 
+def bemoles(escalaMayoresBemoles):
+    escalaElegida = escalasMayoresBemoles[random.randint(0,7)]
+    i = 0
+    print(f"Escribe la escala correspondiente: {escalaElegida[0]} Mayor")
+    for nota in escalaElegida: #Nota ya tiene el valor, no el indice 
+        notaEscala = input()
+        if notaEscala != escalaElegida[i]:
+            print(f"Nota incorrecta: {escalaElegida[i]}")
+        i += 1
+
 escalasMayoresSostenidos = [
     ["Do", "Re", "Mi", "Fa", "Sol", "La", "Si"],
     ["Sol", "La", "Si", "Do", "Re", "Mi", "Fa#"],
